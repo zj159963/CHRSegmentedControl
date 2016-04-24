72d66d06e08c7580e472fc026b0f083d246a229e
Zhang Jian <zj159963@gmail.com>
2016-04-24 21:25:46 +0800

    Initial Commit

---
+++ CHRSegmentedControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CHRSegmentedControl
+//
+//  Created by JianZhang on 16/4/24.
+//  Copyright © 2016年 JianZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CHRSegmentedControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CHRSegmentedControl
+//
+//  Created by JianZhang on 16/4/24.
+//  Copyright © 2016年 JianZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
